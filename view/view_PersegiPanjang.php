a9fdeb94f4a882c4261294394f738b018f7a675d
wahyuni <89575910+sriwahyunipebriyani@users.noreply.github.com>
2023-08-12 21:24:44 +0700

    Update
    update the input field to calculate the area of the rectangle

--- view/view_PersegiPanjang.php
+++ view/view_PersegiPanjang.php
@@ -5,6 +5,17 @@ require '../class/class_perhitungan.php';
 $hitung = new perhitungan();
 
 //menghitung method
-echo $hitung->LuasPersegiPanjang(10, 8);
+// echo $hitung->LuasPersegiPanjang(10, 8);
+
+// membuat kondisi if untuk tomboh hitung
+if (isset($_POST['hitung'])) {
+    // Dibawah ini adalah code pelemparan variabel dari variabel form
+    $panjang = $_POST['panjang'];
+    $lebar = $_POST['lebar'];
+    // Dibawah ini adalah code memanggil fungsi disuatu class melalui object yang di instance
+    echo $hitung->luasPersegi($panjang, $lebar);
+} else {
+    echo "Silahkan Masukan Sisi Pada Persegi Anda.";
+}
 
 ?>
