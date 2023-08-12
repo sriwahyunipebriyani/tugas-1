19faff44af661f792f359bbb47e5f9a6ee4cf1f9
wahyuni <89575910+sriwahyunipebriyani@users.noreply.github.com>
2023-08-12 21:22:17 +0700

    Update
    update the input field to calculate the area of the rectangle. 
    -php native

--- class/class_perhitungan.php
+++ class/class_perhitungan.php
@@ -1,4 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>perhitungan</title>
+</head>
+
+<body>
+
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>masukan panjang</td>
+                <td>:</td>
+                <td><input type="text" name="panjang"></td>
+            </tr>
+            <tr>
+                <td>masukan lebar</td>
+                <td>:</td>
+                <td><input type="text" name="lebar"></td>
+            </tr>
+
+            <tr>
+                <td colspan="3" align="right">
+                    <input type="submit" name="hitung" value="Hitung">
+                </td>
+            </tr>
+
+        </table>
+    </form>
+
+    <?php
 
 class perhitungan
 {
@@ -17,11 +50,13 @@ class perhitungan
 
     public function LuasPersegiPanjang($bilangan1, $bilangan2)
     {
+        echo "contoh :" . "<br>";
         $bilangan3 = $bilangan1 * $bilangan2;
         echo "panjang = $bilangan1" . "<br>";
         echo "lebar = $bilangan2" . "<br>" . "Rumus : " . "<br>";
         echo " L = a x t" . "<br>" . "$bilangan1 x $bilangan2 = ";
-        return "hasil dari luas persegi panjang adalah : " . $bilangan3;
+        return "hasil dari luas persegi panjang adalah : " . $bilangan3 . "<br>" . "<br>";
+
     }
 
     public function LuasSegitiga($bilangan1, $bilangan2)
@@ -33,3 +68,8 @@ class perhitungan
         return "hasil dari luas segitiga adalah : " . $bilangan3;
     }
 }
+?>
+
+</body>
+
+</html>
